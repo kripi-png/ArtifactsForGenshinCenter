79ead1ddc900c9534556298ac149189b9e839db5
kripi-png <66375160+kripi-png@users.noreply.github.com>
2021-10-23 03:50:47 +0300

    first commit

---
+++ src/js/content.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function createSlots() {
+
+}
+
+function createHoverPopup() {
+
+}
+
+function saveToCookies(data) {
+  return
+}
+
+function loadFromCookies() {
+  return
+}
