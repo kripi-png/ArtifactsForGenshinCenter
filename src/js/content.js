1d568028bfa5ab0cfcf9493432b01e14421aa7cc
kripi-png <66375160+kripi-png@users.noreply.github.com>
2021-10-27 00:29:08 +0300

    created editor window

--- src/js/content.js
+++ src/js/content.js
@@ -29,6 +29,7 @@ const createAllSlots = function () {
   });
 }
 
+// create all 5 artifact slots for a single {panel}
 const createSlotsForPanel = function (panel) {
 
   let wrapperDiv = document.createElement('div');
@@ -37,32 +38,138 @@ const createSlotsForPanel = function (panel) {
   let flowerSlot = document.createElement('div');
   flowerSlot.classList.add('artifactSlot', 'flowerSlot');
   flowerSlot.title = "Click here to select a flower!";
+  flowerSlot.addEventListener('click', openArtifactEditor);
   wrapperDiv.appendChild(flowerSlot);
 
   let plumeSlot = document.createElement('div');
   plumeSlot.classList.add('artifactSlot', 'plumeSlot');
   plumeSlot.title = "Click here to select a plume!";
+  plumeSlot.addEventListener('click', openArtifactEditor);
   wrapperDiv.appendChild(plumeSlot);
 
   let sandsSlot = document.createElement('div');
   sandsSlot.classList.add('artifactSlot', 'sandsSlot');
   sandsSlot.title = "Click here to select a sands!";
+  sandsSlot.addEventListener('click', openArtifactEditor);
   wrapperDiv.appendChild(sandsSlot);
 
   let gobletSlot = document.createElement('div');
   gobletSlot.classList.add('artifactSlot', 'gobletSlot');
   gobletSlot.title = "Click here to select a goblet!";
+  gobletSlot.addEventListener('click', openArtifactEditor);
   wrapperDiv.appendChild(gobletSlot);
 
   let circletSlot = document.createElement('div');
   circletSlot.classList.add('artifactSlot', 'circletSlot');
   circletSlot.title = "Click here to select a circlet!";
+  circletSlot.addEventListener('click', openArtifactEditor);
   wrapperDiv.appendChild(circletSlot);
 
 
-
   panel.querySelector('.ItemPanel_itemContent__1D5XB').appendChild(wrapperDiv);
+}
 
+const openArtifactEditor = function (event) {
+  console.log(event.target);
+  let targetArtifactSlot = event.target;
+
+  // THESE CLASSES AND STYLES ARE DIRECTRLY YOINKED FROM THE ORIGINAL WEBSITE
+
+  let editWindow = document.createElement('div');
+  editWindow.id = 'artifactEdit';
+  editWindow.style = 'width: 100vw; z-index: 10000; position: absolute; inset: 0px; pointer-events: all;';
+
+  let editorBackgroudDiv = document.createElement('div');
+  editorBackgroudDiv.classList.add('Window_window__2tU_Y');
+  editWindow.appendChild(editorBackgroudDiv);
+
+  let editorModal = document.createElement('div');
+  editorModal.classList.add('Window_modal__2xmK7');
+
+  let editorWrapper = document.createElement('div');
+  editorWrapper.classList.add('Edit_edit___eEru');
+
+
+  let title = document.createElement('h3');
+  title.innerHTML = "Sayu's Plume"
+  title.style = "background: linear-gradient(160deg, rgba(89, 84, 130, 0.565) 0%, rgba(120, 102, 157, 0.565) 39%, rgba(183, 133, 201, 0.565) 100%);"
+  editorWrapper.appendChild(title);
+
+  /**
+  * ADD ALL ELEMENTS INSIDE editorContent
+  **/
+
+  let editorContent = document.createElement('div');
+  editorContent.classList.add('Edit_content__LoWoP');
+  editorWrapper.appendChild(editorContent);
+
+  let artifactInputWrapper = document.createElement('div');
+  artifactInputWrapper.id = 'artifactInputWrapper';
+
+  let selectArtifactText = document.createElement('p');
+  selectArtifactText.innerHTML = "Select Artifact Set";
+
+  let selectArtifactInput = document.createElement('input');
+  selectArtifactInput.setAttribute('list', 'artifactSelectorDatalist');
+  selectArtifactInput.id = "selectArtifactInput";
+
+  let selectArtifactDatalist = document.createElement('datalist');
+  selectArtifactDatalist.id = 'artifactSelectorDatalist';
+  ['Pale Flame', 'Adventurer', 'Exile'].forEach(a => {
+    let _artifact = document.createElement('option');
+    _artifact.value = a;
+    selectArtifactDatalist.appendChild(_artifact);
+  });
+
+  artifactInputWrapper.appendChild(selectArtifactText);
+  artifactInputWrapper.appendChild(selectArtifactInput);
+  artifactInputWrapper.append(selectArtifactDatalist);
+
+
+  let mainStat = document.createElement('div');
+  mainStat.id = 'mainStatDiv';
+  let mainStatText = document.createElement('p');
+  mainStatText.innerHTML = "Enter Main Stat";
+  let mainStatInput = document.createElement('input');
+  mainStat.appendChild(mainStatText);
+  mainStat.appendChild(mainStatInput);
+
+  let subStat = document.createElement('div');
+  subStat.id = 'subStatDiv';
+  let subStatText = document.createElement('p');
+  subStatText.innerHTML = "Enter Sub Stat";
+  let subStatInput = document.createElement('input');
+  subStat.appendChild(subStatText);
+  subStat.appendChild(subStatInput);
+
+  editorWrapper.appendChild(artifactInputWrapper);
+  editorWrapper.appendChild(mainStat);
+  editorWrapper.appendChild(subStat);
+
+  let editorButton = document.createElement('div');
+  editorButton.classList.add('Edit_buttonWrapper__28k9J');
+  editorWrapper.appendChild(editorButton);
+  editorButton.innerHTML = '<button class="RectButton_button__P4PnK"><div class="RectButton_inner__1BYot"></div><div class="RectButton_border__1xPUl"></div><div class="RectButton_label__1JD0E">OK</div></button>';
+
+  editorModal.appendChild(editorWrapper);
+  editWindow.appendChild(editorModal);
+  document.body.appendChild(editWindow);
+
+  document.querySelector('.Edit_buttonWrapper__28k9J > button.RectButton_button__P4PnK').addEventListener('click', e=>confirmArtifactEdit());
+}
+
+const confirmArtifactEdit = function (event) {
+  let set = document.querySelector('#selectArtifactInput').value;
+  let main = document.querySelector('#mainStatDiv > input').value;
+  let sub = document.querySelector('#subStatDiv > input').value;
+
+  if (!set || !main || !sub) {
+    alert("All fields must be filled!");
+    // return;
+  }
+
+  let _editor = document.querySelector('#artifactEdit');
+  _editor.parentNode.removeChild(_editor); // delete the editor element once artifact is selected
 }
 
 function createHoverPopup() {
