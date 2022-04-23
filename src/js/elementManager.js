aa62cad234f18e4e1d917f6c34af23f51170b697
kripi-png <66375160+kripi-png@users.noreply.github.com>
2022-04-23 03:21:56 +0300

    rewrote createExtensionSettingsSection function

--- src/js/elementManager.js
+++ src/js/elementManager.js
@@ -231,67 +231,66 @@ export const createExtensionSettingsSection = (
   hideAllCheckboxValues, importCallback, exportCallback
 ) => {
   console.log(options_menu);
-  const SECTION_WRAPPER =
-    document.createElement('div');
-    SECTION_WRAPPER.classList.add('PlannerOptions_quickSection__pWVYz');
-    SECTION_WRAPPER.innerHTML =
-      `<div class="${'PlannerOptions_titleWrapper__pAkcp'}">Extension Settings</div>
-      <div class="${'Checkbox_checkbox__yM8Z5'}">
-        <div class="${'Checkbox_buttonWrapper__P_Q_b'}">
-          <button>
-            <svg
-              class="${'Checkbox_checkmark__wYzQF'}"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 52 52">
-              <path
-                id="toggleVisibilityCheckboxPath"
-                class="${'Checkbox_checkmarkCheck__ZKcSz'}"
-                d="m14 27 7 7 16-16"
-                stroke-dashoffset="0px"
-                stroke-dasharray=''>
-              </path>
-            </svg>
-          </button>
-        </div>
-        <h3>Show all artifacts</h3>
+  const STORE_LINK = "https://chrome.google.com/webstore/detail/artifacts-for-genshin-cen/jleonalkkhbfeafkmfgofopiadjkalno";
+  const GITHUB_LINK = "https://github.com/kripi-png/ArtifactsForGenshinCenter";
+
+  const EXTENSION_SETTINGS = document.createElement('div');
+  EXTENSION_SETTINGS.classList.add('PlannerOptions_quickSection__pWVYz');
+  EXTENSION_SETTINGS.innerHTML = `
+    <div class="${'PlannerOptions_titleWrapper__pAkcp'}">Extension Settings</div>
+    <div class="${'Checkbox_checkbox__yM8Z5'}">
+      <div class="${'Checkbox_buttonWrapper__P_Q_b'}">
+        <button>
+          <svg class="${'Checkbox_checkmark__wYzQF'}"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 52 52">
+            <path
+              id="toggleVisibilityCheckboxPath"
+              class="${'Checkbox_checkmarkCheck__ZKcSz'}"
+              d="m14 27 7 7 16-16"
+              stroke-dashoffset="0px"
+              stroke-dasharray=''>
+            </path>
+          </svg>
+        </button>
       </div>
-      <br>
-      <div class="${'Radio_radio__t_pCN'}">
-        <div class="${'Radio_radioLabel__FlU7k'}"" style="text-align: left;">Import / Export artifact data</div>
-        <div class="${'Radio_options__vJPry'} import_export_wrapper" style="max-width: 660px;">
-          <button id="importButton" class="${'Radio_option__6A9gc'}" style="width: 50%;">Import Data</button>
-          <button id="exportButton" class="${'Radio_option__6A9gc'}" style="width: 50%;">Export Data</button>
-        </div>
-      </div>`;
+      <h3>Show all artifacts</h3>
+    </div>
 
-    SECTION_WRAPPER.querySelector('#importButton').onclick = () => importCallback();
-    SECTION_WRAPPER.querySelector('#exportButton').onclick = () => exportCallback();
+    <br>
+    <div class="${'Radio_radio__t_pCN'}">
+      <div class="${'Radio_radioLabel__FlU7k'}"" style="text-align: left;">Import / Export artifact data</div>
+      <div class="${'Radio_options__vJPry'} import_export_wrapper" style="max-width: 660px;">
+        <button id="importButton" class="${'Radio_option__6A9gc'}" style="width: 50%;">Import Data</button>
+        <button id="exportButton" class="${'Radio_option__6A9gc'}" style="width: 50%;">Export Data</button>
+      </div>
+    </div>
 
-    // set the dasharray attribute afterwards as for some reason it
-    // turns to <path stroke-dasharray='0x' [rest as attribute key]>
-    // because of the space
-    SECTION_WRAPPER
-      .querySelector('.' + 'Checkbox_checkmarkCheck__ZKcSz')
-      .setAttribute('stroke-dasharray', hideAllCheckboxValues);
-    // assign the callback function
-    SECTION_WRAPPER.querySelector('button').onclick = () => toggleCallback();
+    <div class="Radio_radioLabel__FlU7k" style="color: #38a6c2">
+      <p>Enjoying the extension?</p>
+      <p>
+        Consider reviewing on <a href=${STORE_LINK}>Webstore</a>
+        or starring on <a href=${GITHUB_LINK}>GitHub</a>.
+        Thanks! <span style="color: #ccc"><3</span>
+      </p>
+    </div>
+  `;
 
-    const reviewNotif =
-      document.createElement('div');
-      reviewNotif.classList.add('Radio_radioLabel__FlU7k');
-      reviewNotif.style.color = '#38a6c2';
-      const storeLink = "https://chrome.google.com/webstore/detail/artifacts-for-genshin-cen/jleonalkkhbfeafkmfgofopiadjkalno";
-      const gitLink = "https://github.com/kripi-png/ArtifactsForGenshinCenter";
-      reviewNotif.innerHTML = `
-        <br>Enjoying the extension?<br>Consider reviewing on
-        <a href=${storeLink}>Webstore</a>
-        or starring on <a href=${gitLink}>GitHub</a>.
-        Thanks! <span style="color: #ccc"><3</span>`;
-      SECTION_WRAPPER.appendChild(reviewNotif);
+  // add callbacks
+  EXTENSION_SETTINGS.querySelector('#importButton').onclick = () => importCallback();
+  EXTENSION_SETTINGS.querySelector('#exportButton').onclick = () => exportCallback();
+  EXTENSION_SETTINGS.querySelector('button').onclick = () => toggleCallback();
+
+  // set the dasharray attribute afterwards as for some reason it
+  // turns to <path stroke-dasharray='0x' [rest as attribute key]>
+  // because of the space
+  EXTENSION_SETTINGS
+    .querySelector('.' + 'Checkbox_checkmarkCheck__ZKcSz')
+    .setAttribute('stroke-dasharray', hideAllCheckboxValues);
 
   // last element is the More Options button
   const options_content = options_menu.querySelector('.PlannerOptions_optionContent__2_jPR');
-  options_content.insertBefore(SECTION_WRAPPER, options_content.lastElementChild);
+  options_content.insertBefore(EXTENSION_SETTINGS, options_content.lastElementChild);
 };
 
 // options_menu:          the dropdown Options-list
