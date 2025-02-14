571f37d56502a65c3c46f1d8fa6b0cffec5c514b
kripi-png <66375160+kripi-png@users.noreply.github.com>
2025-02-14 14:13:49 +0200

    chore: copy config files from a vite project

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { crx } from "@crxjs/vite-plugin";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from "vite";
+import manifest from "./src/manifest.config";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [svelte(), crx({ manifest })],
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      clientPort: 5173,
+    },
+  },
+});
