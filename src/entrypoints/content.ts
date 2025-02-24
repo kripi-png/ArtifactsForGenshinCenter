508a55f3cb1e02e89791410a0c4b642284cf473b
kripi-png <66375160+kripi-png@users.noreply.github.com>
2025-02-24 15:09:13 +0200

    chore: properly use Integrated UI for artifacts

--- src/entrypoints/content.ts
+++ src/entrypoints/content.ts
@@ -1,42 +1,36 @@
 import {
-  createArtifactHidingButtonForPanel,
-  createArtifactSlotsForPanel,
-  getCharacterNameFromPanel,
-  getCharacterPanels,
+  generateArtifactDatalist,
+  generateCharacterObserver,
 } from "@/lib/artifactManager";
-import { getAllArtifactSets, updateLocalDataset } from "@/lib/dataManager";
 import { mount } from "svelte";
 import ExtensionSettings from "../components/ExtensionSettings.svelte";
 import ModalBackdrop from "../components/modals/ModalsBackdrop.svelte";
 import SidepanelOptions from "../components/SidepanelOptions.svelte";
 import "../index.css";
+import { updateLocalDataset } from "@/lib/dataManager";
 
 export default defineContentScript({
   matches: ["https://genshin-center.com/planner"],
   async main(ctx) {
     await updateLocalDataset();
 
-    // TODO: consider and try something like
-    // createIntegratedUi(ctx, {
-    //   anchor: ".Farm_itemList__EgRFB",
-    //   onMount(cont => {
-    //     cont
-    //       .children.map(panel => createIntegratedUi({ anchor: panel }))
-    //       .forEach(childUI => childUI.autoMount ());
-    //   })
-    // });
-
-    const ui = createIntegratedUi(ctx, {
-      position: "inline",
-      anchor: ".Farm_itemList__EgRFB",
-      onMount: (_container) => {
-        main();
-      },
+    // important: backdrop for modals, e.g. the Editor
+    mount(ModalBackdrop, {
+      target: document.body,
     });
 
-    // inject the modifications to the Settings Window
-    // for example the Import / Export buttons
-    const settingsInjection = createIntegratedUi(ctx, {
+    // listen and look for character panels
+    const observer = generateCharacterObserver(ctx);
+    observer.observe(document.body, {
+      subtree: true,
+      childList: true,
+    });
+    ctx.onInvalidated(() => {
+      observer.disconnect();
+    });
+
+    // SETTINGS WINDOW INJECTION
+    createIntegratedUi(ctx, {
       position: "inline",
       anchor: "#options .PlannerOptions_content__kBajJ",
       onMount: (container) => {
@@ -44,10 +38,10 @@ export default defineContentScript({
           target: container,
         });
       },
-    });
+    }).autoMount();
 
-    // inject the modifications to the sidepanel options list
-    const sidePanelOptions = createIntegratedUi(ctx, {
+    // SIDE PANEL OPTIONS LIST INJECTION
+    createIntegratedUi(ctx, {
       position: "inline",
       anchor: ".PlannerOptions_optionContent__2_jPR",
       append: (anchor, ui) => {
@@ -58,49 +52,20 @@ export default defineContentScript({
         mount(SidepanelOptions, {
           target: container,
         });
-      },
-    });
 
-    // Call mount to add the UI to the DOM
-    ui.autoMount();
-    settingsInjection.autoMount();
-    sidePanelOptions.autoMount();
+        if (import.meta.env.DEV) {
+          // _dev_OpenSettingsPanel();
+        }
+      },
+    }).autoMount();
+
+    generateArtifactDatalist();
   },
 });
 
-const main = async () => {
-  // TODO: Do none of this if userArtifactStore.__DISABLED === true
-
-  mount(ModalBackdrop, {
-    target: document.body,
-  });
-
-  // temporarily open the settings panel on reload for convenience
-  // const openSettings: HTMLElement = document.querySelector(
-  //   "div.Farm_farm__uhRH4 > div.Farm_divider__XPwtq div.PlannerOptions_options__t3nvI div > h4",
-  // ) as HTMLElement;
-  // openSettings.click();
-
-  /* pre-generate the datalist for the editor dropdown */
-  const artifactSets = await getAllArtifactSets();
-  const datalist = document.createElement("datalist");
-  datalist.id = "artifactSelectorDatalist";
-  artifactSets.forEach((setName: string) => {
-    const option = document.createElement("option");
-    option.value = setName;
-    datalist.appendChild(option);
-  });
-  document.body.appendChild(datalist);
-
-  /* inject the artifacts and other UI to the character panels */
-  const panels = getCharacterPanels();
-  panels.forEach((panel) => {
-    const characterName = getCharacterNameFromPanel(panel);
-    if (!characterName) {
-      console.error("Character name not found for panel", panel);
-      return;
-    }
-    createArtifactSlotsForPanel(panel, characterName);
-    createArtifactHidingButtonForPanel(panel, characterName);
-  });
+const _dev_OpenSettingsPanel = () => {
+  const openSettings: HTMLElement = document.querySelector(
+    "div.Farm_farm__uhRH4 > div.Farm_divider__XPwtq div.PlannerOptions_options__t3nvI div > h4",
+  ) as HTMLElement;
+  openSettings?.click();
 };
--- src/lib/artifactManager.ts
+++ src/lib/artifactManager.ts
@@ -1,17 +1,15 @@
 import { mount } from "svelte";
-import ArtifactSlotWrapper from "../components/ArtifactSlotWrapper.svelte";
+import type { ContentScriptContext } from "wxt/client";
 import ArtifactDisableButton from "../components/ArtifactDisableButton.svelte";
+import ArtifactSlotWrapper from "../components/ArtifactSlotWrapper.svelte";
+import { getAllArtifactSets } from "./dataManager";
 
 export const createArtifactSlotsForPanel = (
-  panel: HTMLElement,
+  container: HTMLElement,
   characterName: string,
 ) => {
-  const contentWrapper = panel.querySelector(
-    "div.ItemPanel_itemContent__M9oCy",
-  ) as HTMLElement;
-
   mount(ArtifactSlotWrapper, {
-    target: contentWrapper,
+    target: container,
     props: {
       characterName,
     },
@@ -19,14 +17,28 @@ export const createArtifactSlotsForPanel = (
 };
 
 export const createArtifactHidingButtonForPanel = (
-  panel: HTMLElement,
+  container: HTMLElement,
   characterName: string,
 ) => {
   mount(ArtifactDisableButton, {
-    target: panel,
-    anchor: panel.querySelector(".ItemPanel_pauseButton__hI9FU") as HTMLElement,
+    target: container,
     props: { characterName },
   });
+};
+
+export const generateArtifactDatalist = async () => {
+  /* pre-generate the datalist for the editor dropdown */
+  const artifactSets = await getAllArtifactSets();
+  const datalist = document.createElement("datalist");
+  datalist.id = "artifactSelectorDatalist";
+
+  artifactSets.forEach((setName: string) => {
+    const option = document.createElement("option");
+    option.value = setName;
+    datalist.appendChild(option);
+  });
+
+  document.body.appendChild(datalist);
 };
 
 export const getCharacterNameFromPanel = (
@@ -55,4 +67,63 @@ const isPanelWeapon = (panel: HTMLElement): boolean => {
       .querySelector<HTMLElement>("div.ItemPanel_itemImage__ndELA > div")
       ?.style.backgroundImage.includes("weapons"),
   );
+};
+
+/**
+ * Create a mutation observer that listens for character panels.
+ * When the extension loads for the first time, generate an Integrated UI for all existing panels.
+ * @param ctx Content script context from the content script
+ * @returns {MutationObserver} mutation observer that listens for character panels
+ */
+export const generateCharacterObserver = (
+  ctx: ContentScriptContext,
+): MutationObserver => {
+  const mountSlots = (panel: HTMLElement) => {
+    const characterName = getCharacterNameFromPanel(panel);
+    if (!characterName) {
+      console.error("Character name not found for panel", panel);
+      return;
+    }
+    // create and mount artifactWrapper and disabling button
+    createIntegratedUi(ctx, {
+      position: "inline",
+      anchor: panel.querySelector("div.ItemPanel_itemContent__M9oCy"),
+      onMount: (container) => {
+        createArtifactSlotsForPanel(container, characterName);
+      },
+    }).mount();
+    createIntegratedUi(ctx, {
+      position: "inline",
+      anchor: panel.querySelector(".ItemPanel_pauseButton__hI9FU"),
+      append: "before",
+      onMount: (container) => {
+        createArtifactHidingButtonForPanel(container, characterName);
+      },
+    }).mount();
+  };
+
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (mutation.type === "childList") {
+        mutation.addedNodes.forEach((node) => {
+          if ((node as HTMLElement).classList?.contains("Farm_farm__uhRH4")) {
+            // find all characrer panels
+            const panels: HTMLElement[] = [
+              ...(node as HTMLElement).querySelectorAll(
+                ".ItemPanel_item__6lLWZ",
+              ),
+            ] as HTMLElement[];
+
+            console.log(panels);
+            // for each panel, create and mount an integrated ui
+            panels.forEach((panel) => mountSlots(panel));
+          } else if (false) {
+            // if new character is added
+          }
+        });
+      }
+    });
+  });
+
+  return observer;
 };
